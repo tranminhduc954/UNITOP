35dc2f06eaec4f40bd70b8b79c6c8d1c2e3994fe
tranminhduc954 <tranminhduc.hbh@vnpt.vn>
2025-03-04 20:01:53 +0700

    update section 18

--- Backend/PHP/Lession/section18/shopping_cart/modules/cart/add.php
+++ Backend/PHP/Lession/section18/shopping_cart/modules/cart/add.php
@@ -5,6 +5,9 @@ $id = (int)$_GET['id'];
 // Thêm sản phẩm vào giỏ hàng
 add_cart($id);
 
-show_array($_SESSION['cart']);
+// show_array($_SESSION['cart']);
+
+// Chuyển hướng sang giỏ hàng
+redirect('?mod=cart&act=show');
 
 ?>
