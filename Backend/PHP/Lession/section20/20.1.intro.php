d5114c5663562d55d099ada86d4e2dfbd4645112
tranminhduc954 <tranminhduc.hbh@vnpt.vn>
2025-03-08 16:24:11 +0700

    add section20

---
+++ Backend/PHP/Lession/section20/20.1.intro.php
@@ -0,0 +1,10 @@
+<?php
+//1. Khởi động làm quen hệ thống phpMyAdmin
+/**
+ * B1. Khởi động MySQL trên Xampp
+ * B2. Truy cập vào hệ thống: localhost/phpmyadmin
+ */
+
+
+
+?>
---
+++ Backend/PHP/Lession/section20/20.3.create-table.php
@@ -0,0 +1,11 @@
+<?php
+//3. Tạo bảng trong Database
+/**
+ * B1. Chọn database
+ * B2. Click button tạo bảng (create_table)
+ * B3. Chọn tên - số lượng thuộc tính (trường)
+ * B4. Thiết lập thuộc tính kiểu dữ liệu
+ * B5. Tạo bảng
+ */
+
+?>
