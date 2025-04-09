d653184f98e71b6af35d298aaca42ea9af7df195
tranminhduc954 <tranminhduc.hbh@vnpt.vn>
2025-04-09 21:25:16 +0700

    add section23

---
+++ Backend/PHP/Lession/section23/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Làm việc với Ajax</title>
+    <script src="./js/jquery-3.7.1.min.js" type="text/javascript"></script>
+    <script src="./js/app.js" type="text/javascript"></script>
+</head>
+<body>
+    <a id="click_me" href="">Click me</a>
+</body>
+</html>
---
+++ Backend/PHP/Lession/section23/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#click_me").click(function () {
+        alert("clicked");
+    })
+})
